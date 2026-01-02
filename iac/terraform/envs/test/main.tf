b8229e4aaa1144e34145926d2ce4d20a61e82b34
akcabra <andraz.klobucar@gmail.com>
2026-01-02 21:47:45 +0100

    HIKU-31: Dodana pridobitev Azure hostname za api

--- iac/terraform/envs/test/main.tf
+++ iac/terraform/envs/test/main.tf
@@ -80,6 +80,9 @@ resource "azurerm_public_ip" "nat_outbound" {
   location            = azurerm_resource_group.rg.location
   allocation_method   = "Static"
   sku                 = "Standard"
+
+  domain_name_label   = "traefik-${var.project}-${var.environment}"
+
   tags                = local.tags
 }
 
